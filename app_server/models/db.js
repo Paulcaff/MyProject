a60a89b14ec0847916774c15b1433787d5d1be56
Paul Cafferkey <Paul.Cafferkey@students.ittralee.ie>
2019-10-07 13:57:23 +0100

    heroku

--- app_server/models/db.js
+++ app_server/models/db.js
@@ -1,63 +1,63 @@
-// const mongoose = require('mongoose');
-// let dbURI = 'mongodb://localhost/MyProject';
-// if (process.env.NODE_ENV === 'production') {
-//   dbURI = process.env.MONGODB_URI;
-// }
-// mongoose.connect(dbURI);
+const mongoose = require('mongoose');
+let dbURI = 'mongodb://localhost/MyProject';
+if (process.env.NODE_ENV === 'production') {
+  dbURI = process.env.MONGODB_URI;
+}
+mongoose.connect(dbURI);
 
-// const readLine = require ('readline');
-// if (process.platform === 'win32'){
-// const rl = readLine.createInterface ({
-// input: process.stdin,
-// output: process.stdout
-// });
-// rl.on ('SIGINT', () => {
-// process.emit ("SIGINT");
-// });
-// rl.on ('SIGUSR2', () => {
-// process.emit ("SIGUSR2.");
-// });
-// rl.on ('SIGTERM', () => {
-// process.emit ("SIGTERM");
-// });
-// }
+const readLine = require ('readline');
+if (process.platform === 'win32'){
+const rl = readLine.createInterface ({
+input: process.stdin,
+output: process.stdout
+});
+rl.on ('SIGINT', () => {
+process.emit ("SIGINT");
+});
+rl.on ('SIGUSR2', () => {
+process.emit ("SIGUSR2.");
+});
+rl.on ('SIGTERM', () => {
+process.emit ("SIGTERM");
+});
+}
 
-// mongoose.connection.on('connected', () => {
-//   console.log(`Mongoose connected to ${dbURI}`);
-// });
-// mongoose.connection.on('error', err => {
-//   console.log('Mongoose connection error:', err);
-// });
-// mongoose.connection.on('disconnected', () => {
-//   console.log('Mongoose disconnected');
-// });
+mongoose.connection.on('connected', () => {
+  console.log(`Mongoose connected to ${dbURI}`);
+});
+mongoose.connection.on('error', err => {
+  console.log('Mongoose connection error:', err);
+});
+mongoose.connection.on('disconnected', () => {
+  console.log('Mongoose disconnected');
+});
 
-// const gracefulShutdown = (msg, callback) => {
-//   mongoose.connection.close( () => {
-//     console.log(`Mongoose disconnected through ${msg}`);
-//     callback();
-//   });
-// };
+const gracefulShutdown = (msg, callback) => {
+  mongoose.connection.close( () => {
+    console.log(`Mongoose disconnected through ${msg}`);
+    callback();
+  });
+};
 
-// // For nodemon restarts                                 
-// process.once('SIGUSR2', () => {
-//   gracefulShutdown('nodemon restart', () => {
-//     process.kill(process.pid, 'SIGUSR2');
-//   });
-// });
-// // For app termination
-// process.on('SIGINT', () => {
-//   gracefulShutdown('app termination', () => {
-//     process.exit(0);
-//   });
-// });
-// // For Heroku app termination
-// process.on('SIGTERM', () => {
-//   gracefulShutdown('Heroku app shutdown', () => {
-//     process.exit(0);
-//   });
-// });
+// For nodemon restarts                                 
+process.once('SIGUSR2', () => {
+  gracefulShutdown('nodemon restart', () => {
+    process.kill(process.pid, 'SIGUSR2');
+  });
+});
+// For app termination
+process.on('SIGINT', () => {
+  gracefulShutdown('app termination', () => {
+    process.exit(0);
+  });
+});
+// For Heroku app termination
+process.on('SIGTERM', () => {
+  gracefulShutdown('Heroku app shutdown', () => {
+    process.exit(0);
+  });
+});
 
-// require('./team');
+require('./team');
 
     
--- app_server/models/team.js
+++ app_server/models/team.js
@@ -1,26 +1,26 @@
-// const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 
-// const playerSchema = new mongoose.Schema({ 
-//     position:{
-//         type: String,
-//     required: true
-// },
-//     name:{
-//         type: String,
-//     required: true
-// },
-//     weight:{
-//         type: Number,
-//     required: true
-// },
-//     age:{
-//         type: Number,
-//     required: true
-// },
-//     image:{
-//         type: String,
-//     required: true
-// }
-// });
+const playerSchema = new mongoose.Schema({ 
+    position:{
+        type: String,
+    required: true
+},
+    name:{
+        type: String,
+    required: true
+},
+    weight:{
+        type: Number,
+    required: true
+},
+    age:{
+        type: Number,
+    required: true
+},
+    image:{
+        type: String,
+    required: true
+}
+});
 
-// mongoose.model('player', playerSchema);
+mongoose.model('player', playerSchema);
